fcbda0a8d1fd451e81e654bfdf1412e8614fe1f0
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-02-22 23:22:42 +0100

    22.02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Side from './components/Side';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="conteiner">
+      <header>
+          <h1><a href="index.html">Ressursarkiv</a></h1>
       </header>
+
+      <Side/>
+
+      <div id="textCont">
+          <article id="text">
+              <h2>Ressursarkivets start side</h2>
+          </article>
+
+          <article>
+              <ul id="links">
+                  
+              </ul>
+          </article>
+      </div>
     </div>
   );
 }
