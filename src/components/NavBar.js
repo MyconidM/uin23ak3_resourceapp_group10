2c38fa5988c3bbd8e6834f45fea11f127d73591e
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-02-26 22:06:02 +0100

    resurser

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
+import ressurser from './resources/ressurser'; 
+
 export default function NavBar() {
     return (
         <nav className="nav">
-          
+          resources.map()
         </nav>
     )
 }
