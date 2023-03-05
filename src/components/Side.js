cfebbebf736b701704ea6bdacf2df1bd96043b40
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-03-05 01:59:37 +0100

    Route, innhold

--- src/components/Side.js
+++ src/components/Side.js
@@ -1,5 +1,36 @@
-
+import {Link}  from "react-router-dom";
+import Innhold from "./Innhold";
 
 export default function Side() {
-    return 
+    return (
+        <>
+        <header>
+            <h1><a href="index.html">Ressursarkiv</a></h1>
+        </header>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/Html">Html</Link>
+                </li>
+                <li>
+                    <Link to="/CSS">CSS</Link>
+                </li>
+                <li>
+                    <Link to="/JavaScript">JavaScript</Link>
+                </li>
+                <li>
+                    <Link to="/React">React</Link>
+                </li>
+                <li>
+                    <Link to="/HeadlessCMS">Headless cms</Link>
+                </li>
+            </ul>
+        </nav>
+
+      <Innhold/>
+    </>
+    )
 }
